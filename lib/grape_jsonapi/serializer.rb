c02981ade2dd609c6f58dff61921fc00ca5fe4ba
Norifumi Homma <norifumi.homma@gmail.com>
2020-07-16 19:01:03 +0900

    initial commit

---
+++ lib/grape_jsonapi/serializer.rb
@@ -0,0 +1,8 @@
+require "grape_jsonapi/serializer/version"
+
+module GrapeJsonapi
+  module Serializer
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/grape_jsonapi/serializer/version.rb
@@ -0,0 +1,5 @@
+module GrapeJsonapi
+  module Serializer
+    VERSION = "0.1.0"
+  end
+end
